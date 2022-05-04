8c1d7f23ad5268ec668f1acdf790420a4a30de6a
RusterX16 <EliottRuster@gmail.com>
2022-05-04 14:24:24 +0200

    Exportation base de données

--- src/script.sql
+++ src/script.sql
@@ -1,33 +1,69 @@
-DROP DATABASE IF EXISTS pegasemarket;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 04, 2022 at 12:23 PM
+-- Server version: 5.7.36
+-- PHP Version: 8.1.0
 
-CREATE DATABASE pegasemarket;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS requests;
 
-CREATE TABLE IF NOT EXISTS requests
-(
-    email        VARCHAR(64),
-    creationdate DATETIME DEFAULT NOW(),
-    forname    VARCHAR(32) NOT NULL,
-    lastname     VARCHAR(32) NOT NULL,
-    phone        VARCHAR(10) NOT NULL,
-    timeslot     DATETIME    NOT NULL,
-    reminder     DATE        NOT NULL,
-    CONSTRAINT pk_request PRIMARY KEY (email, creationdate),
-    CONSTRAINT pk_valid_timeslot CHECK (timeslot >= CURRENT_DATE),
-    CONSTRAINT pk_valid_reminder CHECK (reminder >= CURRENT_DATE)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-DROP TABLE IF EXISTS timeslot;
+--
+-- Database: `pegasemarket`
+--
 
-CREATE TABLE IF NOT EXISTS timeslot
-(
-    start     TIME,
-    end       TIME,
-    label     VARCHAR(32) NOT NULL,
-    activated BOOL        NOT NULL DEFAULT TRUE,
-    CONSTRAINT pk_timeslot PRIMARY KEY (start, end, label)
-);
+-- --------------------------------------------------------
 
-INSERT INTO timeslot VALUES ('09:00:00', '11:00:00', 'Matin', TRUE);
-INSERT INTO timeslot VALUES ('14:00:00', '16:00:00', 'Apres-Midi', TRUE);
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+CREATE TABLE IF NOT EXISTS `requests` (
+  `email` varchar(64) NOT NULL,
+  `creationdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `forname` varchar(32) NOT NULL,
+  `lastname` varchar(32) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `start` time NOT NULL,
+  `end` time NOT NULL,
+  `reminder` date NOT NULL,
+  PRIMARY KEY (`email`,`creationdate`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timeslot`
+--
+
+DROP TABLE IF EXISTS `timeslot`;
+CREATE TABLE IF NOT EXISTS `timeslot` (
+  `start` time NOT NULL,
+  `end` time NOT NULL,
+  `label` varchar(32) NOT NULL,
+  `activated` tinyint(1) NOT NULL,
+  PRIMARY KEY (`start`,`end`,`label`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `timeslot`
+--
+
+INSERT INTO `timeslot` (`start`, `end`, `label`, `activated`) VALUES
+('09:00:00', '11:00:00', 'Matin', 1),
+('14:00:00', '16:00:00', 'Apres-Midi', 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
